c1ed562b2ad73335dc06a4f29f0511cede2f8bbc
ivanefr <Efrulka2009@gmail.com>
2024-08-01 14:11:04 +0300

    edit text

--- main.py
+++ main.py
@@ -26,14 +26,20 @@ def start(message):
 @bot.callback_query_handler(func=lambda call: call.data == "start")
 def callback_worker(call):
     message = call.message
-    bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id, text="Пришлите ФИО")
+    bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id, text="Введите ваше ФИО.")
     bot.register_next_step_handler(message, get_name)
 
 
 def get_name(message):
     data[message.chat.id] = {}
     data[message.chat.id]["name"] = message.text
-    bot.send_message(chat_id=message.chat.id, text="Введите ваш возвраст")
+    bot.send_message(chat_id=message.chat.id, text="В каком классе вы обучаетесь?")
+    bot.register_next_step_handler(message, get_class)
+
+
+def get_class(message):
+    data[message.chat.id]["class"] = message.text
+    bot.send_message(chat_id=message.chat.id, text="Введите ваш возраст.", )
     bot.register_next_step_handler(message, get_age)
 
 
@@ -45,18 +51,12 @@ def get_courses():
 
 def get_age(message):
     data[message.chat.id]["age"] = message.text
-    keyboard = types.InlineKeyboardMarkup()
-    courses = get_courses()
-    for num, course in enumerate(courses, start=1):
-        button = types.InlineKeyboardButton(text=course, callback_data=f"course{num}")
-        keyboard.add(button)
-    text = "Выберите курс который вы прошли:"
-    bot.send_message(chat_id=message.chat.id, text=text, reply_markup=keyboard)
+    text = "Напишите название курса/интенсива/смены который вы прошли:"
+    bot.send_message(chat_id=message.chat.id, text=text)
+    bot.register_next_step_handler(message, get_course)
 
 
-@bot.callback_query_handler(func=lambda call: call.data.startswith("course"))
-def callback_course(call):
-    message = call.message
+def get_course(message):
     data[message.chat.id]["course"] = message.text
     keyboard = types.InlineKeyboardMarkup()
     yes = types.InlineKeyboardButton(text="Да", callback_data="q1_yes")
@@ -64,57 +64,131 @@ def callback_course(call):
     no = types.InlineKeyboardButton(text="Нет", callback_data="q1_no")
     keyboard.add(no)
 
-    bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id,
-                          text="понравилось?", reply_markup=keyboard)
+    bot.send_message(chat_id=message.chat.id,
+                     text="Проживали ли вы в общежитие во время проведения курса?",
+                     reply_markup=keyboard)
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith("q1"))
 def callback_q1(call):
     message = call.message
     if call.data == "q1_yes":
-        data[message.chat.id]["like"] = True
+        data[message.chat.id]["Проживали ли вы в общежитие во время проведения курса?"] = "Да"
+        keyboard = types.InlineKeyboardMarkup()
+        yes = types.InlineKeyboardButton(text="Да", callback_data="q2_yes")
+        keyboard.add(yes)
+        no = types.InlineKeyboardButton(text="Нет", callback_data="q2_no")
+        keyboard.add(no)
+        bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id,
+                              text="Устраивали ли вас условия проживания?", reply_markup=keyboard)
     else:
-        data[message.chat.id]["like"] = False
+        data[message.chat.id]["Проживали ли вы в общежитие во время проведения курса?"] = "Нет"
+        ask_q3(message)
+
+
+def ask_q3(message, edit=True):
     keyboard = types.InlineKeyboardMarkup()
-    yes = types.InlineKeyboardButton(text="Да", callback_data="q2_yes")
+    yes = types.InlineKeyboardButton(text="Да", callback_data="q3_yes")
     keyboard.add(yes)
-    no = types.InlineKeyboardButton(text="Нет", callback_data="q2_no")
+    no = types.InlineKeyboardButton(text="Нет", callback_data="q3_no")
     keyboard.add(no)
-    bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id,
-                          text="рандомный вопрос??", reply_markup=keyboard)
+    if not edit:
+        bot.send_message(chat_id=message.chat.id, text="Были ли вам полезны лекции?",
+                         reply_markup=keyboard)
+    else:
+        bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id,
+                              text="Были ли вам полезны лекции?", reply_markup=keyboard)
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith("q2"))
 def callback_q2(call):
     message = call.message
-    if call.data == "q1_yes":
-        data[message.chat.id]["q2"] = True
+    if call.data == "q2_yes":
+        data[message.chat.id]["Устраивали ли вас условия проживания?"] = "Да"
+        ask_q3(message)
     else:
-        data[message.chat.id]["q1"] = False
+        data[message.chat.id]["Устраивали ли вас условия проживания?"] = "Нет"
+        bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id,
+                              text="Напишите конкретно что именно вам не понравилось")
+        bot.register_next_step_handler(message, get_dop_info)
+
+
+def get_dop_info(message):
+    data[message.chat.id]["Напишите конкретно что именно вам не понравилось"] = message.text
+    ask_q3(message, edit=False)
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith("q3"))
+def callback_q3(call):
+    message = call.message
+    if call.data == "q3_yes":
+        data[message.chat.id]["Были ли вам полезны лекции?"] = "Да"
+    else:
+        data[message.chat.id]["Были ли вам полезны лекции?"] = "Нет"
+    keyboard = types.InlineKeyboardMarkup()
+    yes = types.InlineKeyboardButton(text="Да", callback_data="q4_yes")
+    keyboard.add(yes)
+    no = types.InlineKeyboardButton(text="Нет", callback_data="q4_no")
+    keyboard.add(no)
     bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id,
-                          text="Напишите общее впечатление о смене/интенсиве")
+                          text="Стали бы вы рекомендовать наши курсы друзьям/знакомым?",
+                          reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith("q4"))
+def callback_q4(call):
+    message = call.message
+    if call.data == "q4_yes":
+        data[message.chat.id]["Стали бы вы рекомендовать наши курсы друзьям/знакомым?"] = "Да"
+    else:
+        data[message.chat.id]["Стали бы вы рекомендовать наши курсы друзьям/знакомым?"] = "Нет"
+    keyboard = types.InlineKeyboardMarkup()
+    yes = types.InlineKeyboardButton(text="Да", callback_data="q5_yes")
+    keyboard.add(yes)
+    no = types.InlineKeyboardButton(text="Нет", callback_data="q5_no")
+    keyboard.add(no)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id,
+                          text="Посетили бы вы другие курсы/смены/интенсивы в Солярисе еще раз?",
+                          reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith("q5"))
+def callback_q5(call):
+    message = call.message
+    if call.data == "q4_yes":
+        data[message.chat.id]["Посетили бы вы другие курсы/смены/интенсивы в Солярисе еще раз?"] = "Да"
+    else:
+        data[message.chat.id]["Посетили бы вы другие курсы/смены/интенсивы в Солярисе еще раз?"] = "Нет"
+    text = """Развернутый отзыв. 🌠
+
+Напишите про ваши впечатления от курса/смены/интенсива. Расскажите, что вам понравилось, а что бы вы улучшили или изменили."""
+    bot.edit_message_text(chat_id=message.chat.id, text=text, message_id=message.message_id)
     bot.register_next_step_handler(message, get_info)
 
 
 def get_info(message):
-    data[message.chat.id]["info"] = message.text
+    data[message.chat.id]["Развёрнутый отзыв"] = message.text
     keyboard = types.InlineKeyboardMarkup()
     buttons = []
     for i in range(1, 6):
-        button = types.InlineKeyboardButton(text=str(i), callback_data=f"q3_{i}")
+        button = types.InlineKeyboardButton(text=str(i), callback_data=f"q7_{i}")
         buttons.append(button)
     keyboard.row(*buttons)
     bot.send_message(chat_id=message.chat.id, text="Оцените в целом мероприятие от 1 до 5",
                      reply_markup=keyboard)
 
 
-@bot.callback_query_handler(func=lambda call: call.data.startswith("q3"))
+@bot.callback_query_handler(func=lambda call: call.data.startswith("q7"))
 def callback_q3(call):
     message = call.message
     data[message.chat.id]["q3"] = int(call.data[-1])
-    bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id,
-                          text="Вам отзыв принят, чтобы оставить еще один нажмите /start")
+    text = """Спасибо за ваш отзыв! 💫
 
+Мы ценим ваше мнение и время, которое вы потратили на опрос. Обратная связь очень важна для улучшения нашей работы. 
+
+Желаем вам удачи в учебе и надеемся увидеть вас снова!"""
+    bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id,
+                          text=text)
 
 
 bot.polling(none_stop=True, interval=0)
